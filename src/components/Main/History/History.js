3a3c00321505104db174052e04f6492978f45e2a
Estevam <estevamfurtado@gmail.com>
2022-03-31 00:12:15 -0300

    Ver historico de habitos

--- src/components/Main/History/History.js
+++ src/components/Main/History/History.js
@@ -6,62 +6,114 @@ import '../../../styles/calendar.css';
 import UserContext from '../../../contexts/UserContext';
 import axios from 'axios';
 import dayjs from 'dayjs';
-
-
 import Main from '../Main';
+
+
+const SelectedDay = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 20px;
+    background-color: white;
+    border-radius: 5px;
+    padding: 10px;
+
+    h1 {
+        font-size: 16px;
+    }
+
+    p {
+        font-size: 14px;
+    }
+`
+const HabitsList = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+`
 
 export default function History({ }) {
 
     const { user, getDayHabits } = useContext(UserContext);
     const [habitsHistory, setHabitsHistory] = useState([]);
 
-    useEffect(() => {
-        const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/history/daily";
-        const config = {
-            headers: { Authorization: `Bearer ${user.token}` }
-        };
+    const [selectedDateData, setSelectedDateData] = useState({ day: null, habits: [] });
 
-        const promise = axios.get(url, config);
-        promise.then(a => {
-            setHabitsHistory(a.data);
-        });
-        promise.catch(e => console.log(e));
-    }, [])
+    useEffect(() => {
+        if (user) {
+            const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/history/daily";
+            const config = {
+                headers: { Authorization: `Bearer ${user.token}` }
+            };
+
+            const promise = axios.get(url, config);
+            promise.then(a => {
+                setHabitsHistory(a.data);
+            });
+            promise.catch(e => console.log(e));
+        }
+    }, [user])
 
     function setClass(date) {
-        
+
         let classColor = null;
 
         let dateStr = dayjs(date).format("DD/MM/YYYY");
         let dateHabits = null;
-
-        let hasHabits = false;
         habitsHistory.forEach(h => {
-            if(h.day === dateStr) {
+            if (h.day === dateStr) {
                 dateHabits = h.habits;
             }
         })
 
         if (dateHabits) {
-            let completion = (dateHabits.filter(h => h.done).length)/dateHabits.length;
-
-            console.log(dateStr, completion);
+            let completion = (dateHabits.filter(h => h.done).length) / dateHabits.length;
 
             if (completion === 1.0) {
                 classColor = "completed"
             } else {
                 classColor = "incompleted"
             }
-        } 
+        }
 
         return classColor;
     }
 
+    function handleClickDay(date) {
+        let dateStr = dayjs(date).format("DD/MM/YYYY");
+        let dateHabits = [];
+        habitsHistory.forEach(h => {
+            if (h.day === dateStr) {
+                dateHabits = h.habits;
+            }
+        })
+        setSelectedDateData({ day: date, habits: [...dateHabits] });
+    }
+
+    const selectedDay = (
+        <SelectedDay>
+            <h1>{dayjs(selectedDateData.day).format("DD/MM/YYYY")}</h1>
+            <HabitsList>
+                {selectedDateData.habits.map(h => {
+                    return (
+                        <p>
+                            {`${h.done ? '✅' : '❌'}   ${h.name}`}
+                        </p>
+                    )
+                })}
+            </HabitsList>
+        </SelectedDay>
+    )
+
     return (
         <Main>
             <h1>Histórico</h1>
-            {/* <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p> */}
-            <Calendar tileClassName={({ date }) => setClass(date)} />
+            <Calendar tileClassName={({ date }) => setClass(date)} onClickDay={(dados) => { handleClickDay(dados) }} />
+            {selectedDateData.habits.length > 0
+                ? selectedDay
+                : <></>
+            }
         </Main>
     )
 }
