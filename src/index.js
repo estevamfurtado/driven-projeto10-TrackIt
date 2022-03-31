a59dd228ae483662416826b23dda6c0d285debc3
Estevam <estevamfurtado@gmail.com>
2022-03-31 17:32:09 -0300

    Ajustando altura no mobile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import ReactDOM from 'react-dom';
 import App from "./components/App";
 
+const appHeight = () => {
+    const body = document.querySelector("body");
+    body.style.setProperty('height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight();
+
+
 const root = document.querySelector(".root");
 ReactDOM.render(
     <App/>
